0b2cf34a0f76fccbbf13a222dc54086f020a839f
tplal <tplalaettin@gmail.com>
2023-01-05 21:28:53 +0300

    main

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#aaa',
+    backgroundColor: '#aba',
     alignItems: 'center',
     justifyContent: 'center',
   },
